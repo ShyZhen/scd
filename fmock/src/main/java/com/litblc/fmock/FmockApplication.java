93c7dda581969a22b0ea7c8f5b40d72b591e5f09
zhenhuaixiu <zhenhuaixiu@sznews.com>
2023-10-17 15:29:26 +0800

    init: 按照scd为父级节点初始化项目

---
+++ fmock/src/main/java/com/litblc/fmock/FmockApplication.java
@@ -0,0 +1,13 @@
+package com.litblc.fmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmockApplication.class, args);
+    }
+
+}
